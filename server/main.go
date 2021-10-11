17ffa92d8ee3f8130f85e075763411d0f39c0995
Gilbert <gilwong3105@gmail.com>
2021-10-10 23:05:12 -0700

    Add more server config

--- server/main.go
+++ server/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	"io"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -15,13 +17,45 @@ func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 
 	// In the future we could report back on the status of our DB, or our cache
 	// (e.g. Redis) by performing a simple PING, and include them in the response.
-	io.WriteString(w, `{"alive": true}`)
+	json.NewEncoder(w).Encode("Yeet")
 }
 
 func main() {
 	r := mux.NewRouter().StrictSlash(true)
-	r.PathPrefix("/api")
+	r.Use(mux.CORSMethodMiddleware(r))
 
-	r.HandleFunc("/health", HealthCheckHandler).Methods("GET")
+	r.HandleFunc("/api/health", HealthCheckHandler).Methods("GET")
+	err := r.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+		pathTemplate, err := route.GetPathTemplate()
+		if err == nil {
+			fmt.Println("ROUTE:", pathTemplate)
+		}
+		pathRegexp, err := route.GetPathRegexp()
+		if err == nil {
+			fmt.Println("Path regexp:", pathRegexp)
+		}
+		queriesTemplates, err := route.GetQueriesTemplates()
+		if err == nil {
+			fmt.Println("Queries templates:", strings.Join(queriesTemplates, ","))
+		}
+		queriesRegexps, err := route.GetQueriesRegexp()
+		if err == nil {
+			fmt.Println("Queries regexps:", strings.Join(queriesRegexps, ","))
+		}
+		methods, err := route.GetMethods()
+		if err == nil {
+			fmt.Println("Methods:", strings.Join(methods, ","))
+		}
+		fmt.Println()
+		return nil
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	http.Handle("/", r)
+
+	fmt.Println("Listening on port 5000. 🚀")
 	log.Fatal(http.ListenAndServe(":5000", r))
 }
